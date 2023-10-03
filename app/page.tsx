60144542320c55c5cb067c1497bc9ccdd5780b52
Divesh Naidu <ddc.divesh@gmail.com>
2023-10-03 01:35:12 +0100

    add delete and add functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
 "use client";
 import cs from "classnames";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const [items, setItems] = useState([
-    { name: "Eggs", done: true },
-    { name: "Dustbin Bags", done: false },
-  ]);
+  let defaultItems = [];
+  if (typeof window !== "undefined") {
+    defaultItems = JSON.parse(localStorage.getItem("items") || "[]");
+  }
+
+  const [items, setItems] = useState(defaultItems);
+  const saveItems = () => {
+    console.log("saving");
+    localStorage.setItem("items", JSON.stringify(items));
+  };
+
+  // useEffect(() => {
+  //   const items = localStorage.getItem("items");
+  //   if (items) {
+  //     setItems(JSON.parse(items));
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    saveItems();
+  }, [items]);
 
   return (
     <main className="min-h-screen min-w-screen animated">
       <div>
-        <div className="p-3 bg-indigo-100">
-          Produce
+        <div className="p-3 bg-indigo-50 w-full border-b-4">
           <form
+            className="text-center"
             onSubmit={(e) => {
               e.preventDefault();
               console.log(e);
+
               setItems([
                 ...items,
                 {
@@ -25,6 +44,10 @@ export default function Home() {
                   done: false,
                 },
               ]);
+
+              // clear input
+              // @ts-ignore
+              (e.target as HTMLFormElement).elements[0].value = "";
             }}
           >
             <input
@@ -32,7 +55,14 @@ export default function Home() {
                 e.preventDefault();
                 console.log(e);
               }}
+              className="outline-none w-1/2  p-1 center rounded-md border-2 border-indigo-200"
             ></input>
+            <button
+              className="bg-indigo-200 p-1 rounded-md border-2 border-indigo-200 color-indigo-800 text-white ml-2"
+              type="submit"
+            >
+              Add
+            </button>
           </form>
         </div>
 
@@ -41,7 +71,7 @@ export default function Home() {
           <div
             key={item.name}
             className={cs(
-              "p-4 border-b-2 transition-all duration-500 overflow-hidden",
+              "p-4 border-b-2 transition-all duration-500 overflow-hidden flex relative pr-10",
               item.done
                 ? "bg-emerald-100 border-emerald-400 text-emerald-800"
                 : "bg-rose-100 border-rose-500 text-rose-950"
@@ -54,11 +84,27 @@ export default function Home() {
           >
             <div
               className={cs(
-                "transition-[width] duration-300 ease-in-out m-r-7 text-right whitespace-nowrap",
+                "transition-[width] duration-300 ease-in-out text-right whitespace-nowrap",
                 item.done ? "w-0" : "w-full"
               )}
             >
               {item.name}
+            </div>
+            <div className="absolute right-3">
+              <Image
+                src="/cross.png"
+                width="20"
+                height="20"
+                alt="trash"
+                className="z-10"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log("delete");
+                  const newItems = [...items];
+                  newItems.splice(i, 1);
+                  setItems(newItems);
+                }}
+              />
             </div>
           </div>
         ))}
